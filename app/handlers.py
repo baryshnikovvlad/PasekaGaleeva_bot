4085e8c8460fb74b6ac999c64c555a1f7fb5a1f0
baryshnikovvlad <baryshnikvlad@gmail.com>
2025-02-19 22:23:35 +0300

    main

--- app/handlers.py
+++ app/handlers.py
@@ -11,7 +11,7 @@ from app.keyboards import main, get_number, YoN, my_callback_data, Assortment, B
 from database.requests import select_users, select_user, insert_user_from_register, NewsLetterStart, \
     edited_text_with_first_name
 from main import bot, log_file_printer, log_file, dp
-from confidential import admin_id, cream_honey_
+from confidential import admin_id, cream_honey_, propolis_, drone_jelly_
 
 router = Router()
 logger = logging.getLogger(__name__)
@@ -65,8 +65,19 @@ async def cream_honey(query: CallbackQuery):
     photo = FSInputFile("content//cream_honey.jpg")
     await bot.send_photo(chat_id=query.from_user.id, photo=photo, caption=cream_honey_[0])
     await bot.send_message(query.from_user.id, text=cream_honey_[1], reply_markup=Back_to_assortment)
-    log_file_printer(f"{query.from_user.id} - assortment: ", log_file)
+    log_file_printer(f"{query.from_user.id} - cream_honey_assortment: ", log_file)
 
+@router.callback_query(my_callback_data.filter(F.data == 'propolis'))
+async def cream_honey(query: CallbackQuery):
+    photo = FSInputFile("content//propolis.jpg")
+    await bot.send_photo(chat_id=query.from_user.id, photo=photo, caption=propolis_[0], reply_markup=Back_to_assortment)
+    log_file_printer(f"{query.from_user.id} - propolis_assortment: ", log_file)
+
+@router.callback_query(my_callback_data.filter(F.data == 'drone_jelly'))
+async def cream_honey(query: CallbackQuery):
+    photo = FSInputFile("content//drone_jelly.jpg")
+    await bot.send_photo(chat_id=query.from_user.id, photo=photo, caption=drone_jelly_[0], reply_markup=Back_to_assortment)
+    log_file_printer(f"{query.from_user.id} - drone_jelly_assortment: ", log_file)
 
 @router.message(NewsLetter.text)
 async def NewsLetter_text(message: Message, state: FSMContext):
